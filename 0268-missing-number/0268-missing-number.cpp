04d1cff5d3e9779dc6520c315c1535814aee1890
prakulgupta01102007-bit <prakulgupta01102007@gmail.com>
2026-08-08 14:07:58 +0530

    Time: 13 ms (10.87%), Space: 28.1 MB (5.16%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_map<int,int>m;
+
+        for(int val : nums) {
+         m[val]++;
+        }
+        for(int i=0;i<=nums.size();i++) {
+            if(m[i] == 0) {
+                return i;
+            }
+        }
+       return 0;
+    }
+};
